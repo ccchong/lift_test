a1993024db50b3adb43e8e2859c4fae07c2261cc
Jason Chong <jason.chong.dev@gmail.com>
2013-02-12 21:29:46 -0500

    Added changes for chapter 3.

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -11,28 +11,52 @@ import Loc._
 import net.liftmodules.JQueryModule
 import net.liftweb.http.js.jquery._
 
+import code.snippet._
+
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
 class Boot {
+  /**
+   * Calculate if the page should be displayed.
+   * In this case, it will be visible every other minute.
+   */
+  def displaySometimes_? : Boolean =
+    (millis / 1000L / 60L) % 2 ==0
+
   def boot {
     // where to search snippet
     LiftRules.addToPackages("code")
 
-    // Build SiteMap
-    val entries = List(
+    def sitemap(): SiteMap = SiteMap(
       Menu.i("Home") / "index", // the simple way to declare a menu
+        Menu.i("Sometimes") / "sometimes" >> If(displaySometimes_? _,
+          S ? "Can't view now"),
+        // A menu with submenus
+        Menu.i("Info") / "info" submenus(
+          Menu.i("About") / "about" >> Hidden >> LocGroup("bottom"),
+          Menu.i("Contact") / "contact",
+            Menu.i("Feeback") / "feedback" >> LocGroup("bottom")
+          ),
+        Menu.i("Sitemap") / "sitemap" >> Hidden >> LocGroup("bottom"),
+        Menu.i("Dynamic") / "dynamic", // a page with dynamic content
+          Param.menu,
+          Menu.param[Which]("Recurse", "Recurse",
+            {case "one" => Full(First())
+            case "two" => Full(Second())
+            case "both" => Full(Both())
+            case _ => Empty },
+            w => w.toString) / "recurse",
 
-      // more complex because this menu allows anything in the
-      // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")))
+          // more complex because this menu allows anything in the
+          // /static path to be visible
+          Menu.i("static") / "static" / **)
 
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    LiftRules.setSiteMapFunc(() => sitemap())
 
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -1,18 +1,18 @@
 package code
 package snippet
 
-import scala.xml.{NodeSeq, Text}
-import net.liftweb.util._
-import net.liftweb.common._
+import lib._
+
+import net.liftweb._
+import util.Helpers._
+import common._
 import java.util.Date
-import code.lib._
-import Helpers._
 
 class HelloWorld {
   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
-  // replace the contents of the element with id "time" with the date
-  def howdy = "#time *" #> date.map(_.toString)
+  def render = "* *" #> date.map(_.toString)
+
 
   /*
    lazy val date: Date = DependencyFactory.time.vend // create the date via factory
